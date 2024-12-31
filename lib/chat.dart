61c0143d1d7b199c12ff8983818bb8560bcc69bb
Biad <biad.engineer@gmail.com>
2024-12-31 17:01:30 +0000

    Add Dialogflow integration and update project configurations

---
+++ lib/chat.dart
@@ -0,0 +1,133 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/chatMessage.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:sound_stream/sound_stream.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({super.key});
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+
+  bool _isRecording = false;
+
+  final RecorderStream _recorder = RecorderStream();
+  late StreamSubscription _recorderStatus;
+  late StreamSubscription<List<int>> _audioStreamSubscription;
+  late BehaviorSubject<List<int>> _audioStream;
+
+// TODO DialogflowGrpc class instance
+  @override
+  void initState() {
+    super.initState();
+    initPlugin();
+  }
+
+  @override
+  void dispose() {
+    _recorderStatus?.cancel();
+    _audioStreamSubscription?.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlugin() async {
+    _recorderStatus = _recorder.status.listen((status) {
+      if (mounted) {
+        setState(() {
+          _isRecording = status == SoundStreamStatus.Playing;
+        });
+      }
+    });
+
+    await Future.wait([_recorder.initialize()]);
+
+    // TODO Get a Service account
+  }
+
+  void stopStream() async {
+    await _recorder.stop();
+    await _audioStreamSubscription?.cancel();
+    await _audioStream?.close();
+  }
+
+  void handleSubmitted(text) async {
+    print(text);
+    _textController.clear();
+
+    //TODO Dialogflow Code
+  }
+
+  void handleStream() async {
+    _recorder.start();
+
+    _audioStream = BehaviorSubject<List<int>>();
+    _audioStreamSubscription = _recorder.audioStream.listen((data) {
+      print(data);
+      _audioStream.add(data);
+    });
+
+    // TODO Create SpeechContexts
+    // Create an audio InputConfig
+
+    // TODO Make the streamingDetectIntent call, with the InputConfig and the audioStream
+    // TODO Get the transcript and detectedIntent and show on screen
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          ),
+        ),
+        Divider(height: 1.0),
+        Container(
+          decoration: BoxDecoration(color: Theme.of(context).cardColor),
+          child: IconTheme(
+            data: IconThemeData(color: Theme.of(context).canvasColor),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      controller: _textController,
+                      onSubmitted: handleSubmitted,
+                      decoration:
+                          InputDecoration.collapsed(hintText: "Send a message"),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () => handleSubmitted(_textController.text),
+                    ),
+                  ),
+                  IconButton(
+                    iconSize: 30.0,
+                    icon: Icon(_isRecording ? Icons.mic_off : Icons.mic),
+                    onPressed: _isRecording ? stopStream : handleStream,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/chatMessage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage(
+      {super.key, required this.text, required this.name, required this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: const CircleAvatar(child: Text('B')),
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(name, style: const TextStyle(fontWeight: FontWeight.bold)),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: Text(text),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(this.name, style: Theme.of(context).textTheme.labelMedium),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: Text(text),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: CircleAvatar(
+            child: Text(
+          name[0],
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        )),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+  }
+}
